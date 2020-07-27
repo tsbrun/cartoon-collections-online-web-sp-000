dd1ae44f09818d48441e0a5d62a28df248fd1fb7
Anya Brun <tsbrun@icloud.com>
2020-07-27 19:36:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(planeteer_calls)
-  planeteer_calls.any? { |planeteer_call| planeteer_call.length > 4 }
+  planeteer_calls.any? { |call| call.length > 4 }
 end
 
 
